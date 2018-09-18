1d743c6904c91af00cdd6c91ae6a65ad69a0e56a
Maximiliano Battafarano <batatin.92@gmail.com>
2018-09-17 21:33:54 -0300

    Se agrega securizacion

--- invesmart-web/src/main/java/com/invesmart/invesmartWeb/InvesmartWebApplication.java
+++ invesmart-web/src/main/java/com/invesmart/invesmartWeb/InvesmartWebApplication.java
@@ -5,6 +5,12 @@ import com.invesmart.invesmartWeb.configuration.InvesmartWebConfiguration;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class InvesmartWebApplication {
@@ -15,6 +21,18 @@ public class InvesmartWebApplication {
                 .sources(InvesmartCoreApplication.class, InvesmartWebApplication.class)
                 .properties(InvesmartWebConfiguration.getWebConfiguration())
                 .run(args);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers","Access-Control-Allow-Origin","Access-Control-Request-Method", "Access-Control-Request-Headers","Origin","Cache-Control", "Content-Type", "Authorization"));
+        configuration.setAllowedMethods(Arrays.asList("DELETE", "GET", "POST", "PATCH", "PUT"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
